400acb2b44bdd48655e501d0fed8c2bd5d36f15c
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-11-28 16:39:54 -0500

    Created Game Manager for scene

--- DevCon3/Assets/Scripts/BallBehaviour.cs
+++ DevCon3/Assets/Scripts/BallBehaviour.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
-    [SerializeField] private int playerNum;
     [SerializeField] public Rigidbody2D rb2D {  get; private set; }
     [SerializeField] public KeyCode launchKey { get; private set; } = KeyCode.Space;
 
@@ -12,20 +11,21 @@ public class BallBehaviour : MonoBehaviour
 
     public Transform ballPos;
     public GameObject[] paddleObject;
-    private Vector2[] startingPos;
+    [SerializeField] public Vector2[] startingPos {  get; private set; }
 
     //Physic calculation variables
     [SerializeField] private float gravity = 9.81f; 
     private float mass = 0.0594f; //average tennis ball mass (kg)
     private float frictionCoefficient = 0.8f; //friction coefficient assuming level is made with hard court material
-    [SerializeField] public float appliedForce = 0.5f;
+    [SerializeField] public float appliedForce = 0.05f;
     private float bounceForce = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        ballPos = transform;
+
         isLaunched = false;
-        playerNum = Random.Range(1, 3);
         startingPos = new Vector2[]
         {
             new Vector2(paddleObject[0].transform.position.x + 2, paddleObject[0].transform.position.y),
@@ -35,7 +35,7 @@ public class BallBehaviour : MonoBehaviour
         //StartingPositions();
 
         rb2D = GetComponent<Rigidbody2D>();
-        LaunchBall();
+        //LaunchBall();
 
         //Vector2 weight = new Vector2(0, mass * gravity); //Weight calculation for ball
         //rb2D.AddForce(weight, ForceMode2D.Impulse);
@@ -49,33 +49,23 @@ public class BallBehaviour : MonoBehaviour
     void LaunchBall()
     {
         //Launch ball in random direction
-        float x = Random.Range(0, 2);
-        float y = Random.Range(-2, 2);
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(-1, 1);
 
         Vector2 launch = new Vector2(x, y).normalized;
         rb2D.velocity = launch * appliedForce;
     }
 
-    void StartingPositions()
-    {
-        //Ball starts at either player 1 or 2 positions
-        if(playerNum == 1)
-        {
-            ballPos.position = startingPos[0];
-        }
-        else
-        {
-            ballPos.position = startingPos[1];
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Walls"))
         {
             //Makes sure that the paddles applies a force 
             Vector2 paddle = collision.rigidbody.velocity;
             rb2D.velocity += paddle * appliedForce;
+
+            Quaternion rotation = Quaternion.Euler(0, 0, 1);
+            rb2D.transform.rotation = rotation;
         }
     }
 }
--- DevCon3/Assets/Scripts/GameManager.cs
+++ DevCon3/Assets/Scripts/GameManager.cs
@@ -4,16 +4,71 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Create game manager variable to reference itself
+    public static GameManager manager;
+
+    //Enumerator that keeps track of player turns
+    public enum playerTurn {player1, player2};
+
+    //A variable that keeps track of which turn to start
+    public playerTurn currentTurn;
+
+    private bool hasGameStarted = false;
+
+    [SerializeField] private int randomStart;
+
+    BallBehaviour ball;
+
+    private void Awake()
+    {
+        //If the game manager doesn't exist, make this the game manager
+        if(manager == null)
+        {
+            manager = this;
+
+            //Make the game object persistent throughout play
+            DontDestroyOnLoad(gameObject);
+        }
+
+        //Else destroy the game object if it already exists
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ball = FindObjectOfType<BallBehaviour>(); //Reference the ball behaviour script
+
+        //At the start of the game, randomly generate number
+        randomStart = Random.Range(1, 3);
+
+        //If the number generated is 1, its player 1's turn
+        if(randomStart == 1)
+        {
+            currentTurn = playerTurn.player1;
+        }
+
+        //If the number generated is 2, its player 2's turn
+        if (randomStart == 2)
+        {
+            currentTurn = playerTurn.player2;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentTurn)
+        {
+            case playerTurn.player1:
+                ball.ballPos.position = ball.startingPos[0];
+                break;
+            case playerTurn.player2:
+                ball.ballPos.position = ball.startingPos[1];
+                break;
+        }
     }
 }
