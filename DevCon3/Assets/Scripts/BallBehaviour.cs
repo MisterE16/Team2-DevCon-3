5d513c9764ab7197a537f337352f9c224b707f9a
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-11-27 19:31:30 -0500

    apply force onto ball

--- DevCon3/Assets/Scripts/BallBehaviour.cs
+++ DevCon3/Assets/Scripts/BallBehaviour.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     [SerializeField] private int playerNum;
+    [SerializeField] public Rigidbody2D rb2D {  get; private set; }
+    [SerializeField] public KeyCode launchKey { get; private set; } = KeyCode.Space;
+
+    private bool isLaunched;
 
     public Transform ballPos;
     public GameObject[] paddleObject;
     private Vector2[] startingPos;
 
+    //Physic calculation variables
+    [SerializeField] private float gravity = 9.81f; 
+    private float mass = 0.0594f; //average tennis ball mass (kg)
+    private float frictionCoefficient = 0.8f; //friction coefficient assuming level is made with hard court material
+
     // Start is called before the first frame update
     void Start()
     {
+        isLaunched = false;
         playerNum = Random.Range(1, 3);
         //startingPos = new Vector2[]
         //{
         //    new Vector2(paddleObject[0].transform.position.x + 2, paddleObject[0].transform.position.y),
         //    new Vector2(paddleObject[1].transform.position.x - 2, paddleObject[1].transform.position.y)
         //};
+
+        rb2D = GetComponent<Rigidbody2D>();
+
+        Vector2 weight = new Vector2(0, mass * gravity); //Weight calculation for ball
+        rb2D.AddForce(weight, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //StartingPositions();
-        BallPhysics();
+        if(Input.GetKey(launchKey))
+        {
+            //StartingPositions();
+            BallPhysics();
+        }
     }
 
     void BallPhysics()
     {
-
+        Vector2 applyForce = Vector2.right;
+        rb2D.AddForce(applyForce, ForceMode2D.Impulse);
     }
 
     void StartingPositions()
